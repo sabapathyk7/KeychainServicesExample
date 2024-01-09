d2e90f43e4fdfdb1f4f07dadaf91790c5ae37a85
Kanagasabapathy Rajkumar <40764138+sabapathyk7@users.noreply.github.com>
2024-01-09 16:22:16 +0530

    Initial Commit

---
+++ KeychainServicesExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeychainServicesExample
+//
+//  Created by kanagasabapathy on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

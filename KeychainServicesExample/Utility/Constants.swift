3cc76312c8cf0e518045aa628d6aa10f8d7977a6
Kanagasabapathy <trksabapathy7@gmail.com>
2024-01-09 22:51:39 +0530

    Constants

---
+++ KeychainServicesExample/Utility/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  KeychainServicesExample
+//
+//  Created by kanagasabapathy on 09/01/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let saveDataStr              = "Save Data to Keychain"
+    static let fetchDataStr             = "Fetch Data from Keychain"
+    static let updateDataStr            = "Update Data to Keychain"
+    static let deleteDataStr            = "Delete Data from Keychain"
+
+    static let usernamePlaceHder        = "Enter Username"
+    static let passwordPlaceHder        = "Enter Password"
+
+    static let updatePasswordPlaceHder  = "Enter Password to Update"
+
+    static let fetched                  = "Fetched Password: "
+    static let saved                    = "Saved Password: "
+    static let deleted                  = "Deleted Username: "
+    static let updated                  = "Updated Password: "
+    static let noMessage                = "no message"
+
+}
